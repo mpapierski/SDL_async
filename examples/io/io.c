83ffe0ac8628ed8e69611b46586b00fb5518e5ba
Michał Papierski <michal@papierski.net>
2013-03-22 00:35:26 +0100

    SDL initialized in the asynchronous I/O example.

--- examples/io/io.c
+++ examples/io/io.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <SDL_async/SDL_async.h>
+#include <SDL.h>
+
 int
 main()
 {
-	puts("Hello world!");
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+	atexit(SDL_Quit);
+	Async_Init();
+	atexit(Async_Free);
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+	}
 	return 0;
 }
--- src/async.c
+++ src/async.c
@@ -1,5 +1,13 @@
 #include <SDL_async/SDL_async.h>
 
-void Async_Queue_Work(void * baton)
+void Async_Init()
+{
+}
+
+void Async_Free()
+{
+}
+
+void Async_Queue_Work(async_function_t task, void * baton)
 {
 }
